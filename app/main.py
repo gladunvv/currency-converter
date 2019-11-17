6fbbd77174830c647a9ef7efdf288d1cd65959ad
vladislav <bincha.1997@gmail.com>
2019-11-17 22:43:28 +0300

    Update

--- app/main.py
+++ app/main.py
@@ -1,42 +1,39 @@
 import socket
 import json
+import requests
 
+# Where USD is the base currency you want to use
+
+# Making our request
+
+
+# Your JSON object
+		
 
 URLS = {
-    '/': {'HEllo': 'BUYs'},
+    '/': {'RUB': '0'},
 }
 
 
-def parse_request(request):
-    parsed = request.split(' ')
-    method = parsed[0]
-    url = parsed[1]
-    return method, url
+def get_currency(convert):
 
-def genreate_headers(method, url):
-    if not method == 'GET':
-        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
-
-    if not url in URLS:
-        return ('HTTP/1.1 404 Not found\n\n', 404)
-
-    return ('HTTP/1.1 200 OK\n\n', 200)
-
-def genreate_content(code, url):
-    if code == 404:
-        return '<h1>Page not found 404</h1>'
-
-    if code == 405:
-        return '<h1>Method not allowd 405</h1>'
-
-    return json.dumps(URLS[url])
+    api_key = b66c5759f6c7452f3e98
+    
+    if 'RUB:' in convert.keys():
+        url = 'https://www.currencyconverterapi.com/api/v7/convert?q=RUB_USD&compact=ultra&apiKey={}'.format(
+            api_key,
+        )
+        response = requests.get(url)
+        data = response.json()
+        data = int(data['USD_RUB']) * convert['RUB:']
+        return data
+    #elif 'USD' in convert.keys():
 
 
-def generate_response(request):
-    method, url = parse_request(request)
-    headers, code = genreate_headers(method, url)
-    body = genreate_content(code, url)
-    return (headers + body).encode()
+
+
+
+
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,15 +44,59 @@ def main():
     while True:
         client_socket, addr = server_socket.accept()
         request = client_socket.recv(1024)
-        print(request)
-        print()
-        print(addr)
+        #print(request)
+        #print()
+        #print(addr)
 
         response =  generate_response(request.decode('UTF-8'))
 
         client_socket.sendall(response)
         client_socket.close()
 
+def generate_response(request):
+    method, url, convert = parse_request(request)
+    headers, code = genreate_headers(method, url)
+    body = genreate_content(code, url, convert)
+    return (headers + body).encode()
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    res = parsed[3]
+    convert_type = parsed[2].split('\n')
+    convert_type = convert_type[1]
+    print(convert_type)
+    currency = res.split('\r')
+    convert = {convert_type: currency}
+    return method, url, convert
+
+def genreate_headers(method, url):
+
+    if not method == 'POST':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+
+
+
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def genreate_content(code, url, convert):
+    if code == 404:
+        return '<h1>404</h1><p>Page not found</p>'
+
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowd</p>'
+
+    if 'RUB:' and 'USD:' not in convert.keys():
+        return json.dump({'errors': 'not found data'})
+
+    data = get_currency(convert)
+    URLS[url]['RUB'] = data
+    return json.dumps(URLS[url])    
 
 
 if __name__ == "__main__":
